1b0df63d06c2a05edf343e8b6a267269ac6b649f
PoK728 <siaziawiktor@gmail.com>
2026-06-06 02:03:01 +0200

    dodano sortowanie tabeli oraz generowanie terminarza

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 
 public class Main {
     public static void main(String[] args){
-       SezonPilkarski s1 = new SezonPilkarski("essa1");
-       Klub k1 = new Klub(1, "Jacek Fc");
-       Klub k2 = new Klub(2, "Jaga");
-       s1.zarejestrujKlub(k1);
-       s1.zarejestrujKlub(k2);
-       //s1.generujTabele();
-       Spotkanie sp1 = new Spotkanie(k1, k2);
-    //    sp1.wprowadzWynik(1, 3);
-       sp1.pobierzZwyciezce();
-       s1.generujTabele();
+      SezonPilkarski s1 = new SezonPilkarski("essa1");
+      Klub k1 = new Klub(1, "Jacek Fc");
+      Klub k2 = new Klub(2, "Jaga");
+      s1.zarejestrujKlub(k1);
+      s1.zarejestrujKlub(k2);
+      //s1.generujTabele();
+      Spotkanie sp1 = new Spotkanie(k1, k2);
+      sp1.wprowadzWynik(1, 3);
+      sp1.pobierzZwyciezce();
+      s1.generujTabele();
 
     }
     
--- SezonPilkarski.java
+++ SezonPilkarski.java
@@ -16,14 +16,48 @@ public class SezonPilkarski {
     }
 
     public void generujTerminarzSezonu(){
-
+        List<Klub> listaZapasowa = new ArrayList<>(listaKlubow);
+        int liczbaMeczow = listaZapasowa.size()/2;
+        KolejkaLigowa k1 = new KolejkaLigowa(1);
+        for(int i=0;i<liczbaMeczow;i++){
+            Klub gosp = listaZapasowa.get(i);
+            Klub gosc = listaZapasowa.get(listaZapasowa.size()-1-i);
+            Spotkanie spotkanie = new Spotkanie(gosp, gosc);
+            k1.dodajMecz(spotkanie);
+        }
+            
+            
     }
 
     public void generujTabele(){
+        boolean informator;
         System.out.println("Klub | pkt");
-        for(Klub kluby : listaKlubow){
-            System.out.println(kluby.pobierzNazwe() + " | " + kluby.pobierzPunkty());
-        }
+        //ponizej sortowanie tabeli po punktach, jezeli beda takie same warotsci dla pkt, to po nazwie
+        do{
+            informator=false;
+            for(int i=0;i<listaKlubow.size()-1;i++){
+            Klub pierwszy = listaKlubow.get(i);
+            Klub drugi = listaKlubow.get(i+1);
+            if(drugi.pobierzPunkty() > pierwszy.pobierzPunkty()){
+                listaKlubow.set(i,drugi);
+                listaKlubow.set(i+1,pierwszy);
+                informator=true;
+            }
+            if(pierwszy.pobierzPunkty()==drugi.pobierzPunkty()){
+                if(pierwszy.pobierzNazwe().compareTo(drugi.pobierzNazwe())>0){
+                    listaKlubow.set(i,drugi);
+                    listaKlubow.set(i+1,pierwszy);
+                    informator=true;
+                }
+            }
+            
+            }
+        }while(informator==true);
+        for(Klub kluby:listaKlubow){
+                System.out.println(kluby.pobierzNazwe()+" | "+kluby.pobierzPunkty()); // wypisanie tabeli
+            }
+        
+        
     }
 
     public void zapiszStanSezonu(String sciezka){
